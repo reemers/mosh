d140e70b23b227dfcdb5ec380386efbf70606bbb
reem <mausty4@gmail.com>
2023-11-06 12:25:11 -0500

    updated code

--- mosh/src/components/counter.jsx
+++ mosh/src/components/counter.jsx
@@ -1,10 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {  } 
-    render() { 
-        return ();
-    }
+  state = {
+    count: 0,
+  };
+
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleIncrement = () => {
+    console.log("Increment Clicked", this);
+  };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return;
+    <ul>
+      {this.state.tags.map((tag) => (
+        <li key={tag}>{tag}</li>
+      ))}
+    </ul>;
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
+      </div>
+    );
+  }
 }
- 
+
 export default Counter;
--- mosh/src/index.js
+++ mosh/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
 
-const element = <h1>Hello World</h1>;
-
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<Counter />, document.getElementById("root"));
