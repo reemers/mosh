f7a7daa6762d869b912887a65286d883c96025a9
reem <mausty4@gmail.com>
2023-11-06 09:33:23 -0500

    initial commit

---
+++ mosh/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+
+const element = <h1>Hello World</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
